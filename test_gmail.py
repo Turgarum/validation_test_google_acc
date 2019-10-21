161612e271c0d9a97fcf6642391131a4daabda5c
Turgarum <turgarum@gmail.com>
2019-10-21 12:23:30 +0300

    Update

--- test_gmail.py
+++ test_gmail.py
@@ -13,6 +13,7 @@ time.sleep(1)
 
 for_myself_button = driver.find_element_by_xpath("//*[text()='Для себя']")
 for_myself_button.click()
+validation_error_message = 'Имя пользователя может включать латинские буквы (a-z), цифры (0-9) и точку (.).'
 
 first_name_field = driver.find_element_by_id("firstName")
 last_name_field = driver.find_element_by_id("lastName")
@@ -23,18 +24,18 @@ confirm_password_field = driver.find_element_by_name("ConfirmPasswd")
 username_field = driver.find_element_by_id("username")
 next_button = driver.find_element_by_id("accountDetailsNext")
 email_list = {'@a.a', 'a@-a.a', 'a@a@a.a', 'a@a'}
+user_dictionary = { 'fn':'Sergey', 'ln':'Cheshkov', 'password':'Abc123456!' }
 
-
-first_name_field.send_keys("Sergey")
-last_name_field.send_keys("Cheshkov")
-password_field.send_keys("Abc123456!")
-confirm_password_field.send_keys("Abc123456!")
+first_name_field.send_keys(user_dictionary['fn'])
+last_name_field.send_keys(user_dictionary['ln'])
+password_field.send_keys(user_dictionary['password'])
+confirm_password_field.send_keys(user_dictionary['password'])
 
 for email in email_list:
     username_field.clear()
     username_field.send_keys(email)
     next_button.click()
     time.sleep(1)
-    assert 'Имя пользователя может включать латинские буквы (a-z), цифры (0-9) и точку (.).' in driver.page_source
+    assert validation_error_message in driver.page_source
 
 driver.close()
