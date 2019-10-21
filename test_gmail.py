5e289f3164d11576084fc06ae660f148da0abf51
Turgarum <turgarum@gmail.com>
2019-10-21 12:06:17 +0300

    Simple test with cycle created.

--- test_gmail.py
+++ test_gmail.py
@@ -9,19 +9,33 @@ create_account_button = driver.find_element_by_xpath("//*[text()='Создать
 create_account_button.click()
 
 time.sleep(1)
+#driver.implicitly_wait(1)
 
 for_myself_button = driver.find_element_by_xpath("//*[text()='Для себя']")
 for_myself_button.click()
 
 first_name_field = driver.find_element_by_id("firstName")
-first_name_field.send_keys("Sergey")
-
 last_name_field = driver.find_element_by_id("lastName")
-last_name_field.send_keys("Cheshkov")
-
 password_field = driver.find_element_by_name("Passwd")
-password_field.send_keys("Abc123456!")
-
 confirm_password_field = driver.find_element_by_name("ConfirmPasswd")
+
+
+username_field = driver.find_element_by_id("username")
+next_button = driver.find_element_by_id("accountDetailsNext")
+email_list = {'@a.a', 'a@-a.a', 'a@a@a.a', 'a@a'}
+
+
+first_name_field.send_keys("Sergey")
+last_name_field.send_keys("Cheshkov")
+password_field.send_keys("Abc123456!")
 confirm_password_field.send_keys("Abc123456!")
 
+for email in email_list:
+    username_field.clear()
+    username_field.send_keys(email)
+    next_button.click()
+    time.sleep(1)
+    assert 'Имя пользователя может включать латинские буквы (a-z), цифры (0-9) и точку (.).' in driver.page_source
+
+time.sleep(3)
+driver.close()
